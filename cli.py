c6c9ac4576f2236480a1afbee46410fc3eedc9af
alakoth <76724219+alakoth@users.noreply.github.com>
2022-01-20 04:41:57 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,6 +2,7 @@ from main import generate
 import argparse 
 parser = argparse.ArgumentParser(description='ETERNALCRYSTAL - Generate code names on the fly.')
 parser.add_argument('-w', metavar='--wordlist', type=str, nargs='?',help='path to wordlist')
+parser.add_argument('-v', '--version', action='version', version='v0.0.1-alpha', help='print version')
 args = parser.parse_args()
 file = f'{args.w}'
 generate(file)
